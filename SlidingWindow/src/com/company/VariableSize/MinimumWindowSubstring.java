e2845d803362823acc043ff555ef39c4c26630aa
sohail60 <sohailatwork10@gmail.com>
2023-01-30 10:36:21 +0530

    Updated

--- SlidingWindow/src/com/company/VariableSize/MinimumWindowSubstring.java
+++ SlidingWindow/src/com/company/VariableSize/MinimumWindowSubstring.java
@@ -4,51 +4,47 @@ import java.util.HashMap;
 
 public class MinimumWindowSubstring {
     public static void main(String[] args) {
-        String str="timetopractice",ptr = "toc";
-        System.out.println(mimimumWindowSubstring(str,ptr));
+        String str = "timetopractice", ptr = "toc";
+        System.out.println(mimimumWindowSubstring(str, ptr));
     }
 
-    static String mimimumWindowSubstring(String str, String ptr){
-        int n=str.length();
-        int l=ptr.length();
-        int i=0,j=0,count=0,min=0;
-        String result="";
-        HashMap<Character,Integer> map=new HashMap<>();
+    static String mimimumWindowSubstring(String str, String ptr) {
+        int n = str.length();
+        int l = ptr.length();
+        int i = 0, j = 0, min = Integer.MAX_VALUE;
+        String result="-1";
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for (int m = 0; m < l; m++) {
-            map.put(ptr.charAt(m),map.getOrDefault(ptr.charAt(m),0)+1);
+            map.put(ptr.charAt(m), map.getOrDefault(ptr.charAt(m), 0) + 1);
         }
 
-        int size=map.size();
-        while(j<n){
-            char ch=str.charAt(j);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==0){
+        int size = map.size();
+        while (j < n) {
+            char ch = str.charAt(j);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) == 0) {
                     size--;
                 }
             }
 
-            if(size<0){
-                j++;
-            } else if(size==0){
-                if(j-i+1<min){
-                min=j-i+1;
-                result=str.substring(i,j+1);
-                }
-                j++;
-            } else if(size>0){
-                while (size>0){
-                    if(map.containsKey(str.charAt(i))){
-                        map.put(str.charAt(i),map.get(str.charAt(i))+1);
-                        if(map.get(str.charAt(i))==1){
+            if (size == 0) {
+                while (size == 0) {
+                    if (j - i + 1 < min) {
+                        min = j - i + 1;
+                        result = str.substring(i, j + 1);
+                    }
+                    if (map.containsKey(str.charAt(i))) {
+                        map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+                        if (map.get(str.charAt(i)) == 1) {
                             size++;
                         }
                     }
                     i++;
                 }
-                j++;
             }
+            j++;
         }
         return result;
     }
